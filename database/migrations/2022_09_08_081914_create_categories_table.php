a868df7514a2f49361ba774469e3406bc66c2edd
Asif Ahmed <asifahmed1132@gmail.com>
2022-09-11 00:25:42 +0600

    migration

--- database/migrations/2022_09_08_081914_create_categories_table.php
+++ database/migrations/2022_09_08_081914_create_categories_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('category_name');
+            $table->boolean('status')->default(1);
             $table->timestamps();
         });
     }
